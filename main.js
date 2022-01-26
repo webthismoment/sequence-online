12d5ad749284de7ad3fc697fee5419fc73741cc7
webthismoment <85307010+webthismoment@users.noreply.github.com>
2022-01-26 22:47:33 +0900

    socket.join

--- main.js
+++ main.js
@@ -3,12 +3,18 @@ const express = require("express");
 const http = require("http");
 
 const app = express();
-//app.get("/", 
 app.get("/client", (req, res) => res.sendFile(`${__dirname}/client/client.html`));
 const server = http.createServer(app);
+server.listen(5500, () => console.log("#### 서버 시작 ####"));
+
+//socket 
+let player_cnt = 0;
 
 const io = socketio(server);
 io.on("connection", (socket) => {
+    //socket.join(`${}`);
+    //io.to("room1").to("room2").to("room3").emit("some event");
+    
     const { url } = socket.request;
     console.log(`연결됨: ${url}`);
 
@@ -26,7 +32,6 @@ io.on("connection", (socket) => {
     })
 });
 
-server.listen(5500, () => console.log("#### 서버 시작 ####"));
 
 // var wiki = require('./wiki.js')
 // app.use('/wiki', wiki)
