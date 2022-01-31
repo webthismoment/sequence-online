151ca49c05cb0184205b24925ac7a70a60704b32
webthismoment <85307010+webthismoment@users.noreply.github.com>
2022-01-31 18:06:21 +0900

    express starter

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
-const socketio = require("socket.io");
+//const socketio = require("socket.io");
 const express = require("express");
 const http = require("http");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+//var usersRouter = require("./routes/users");
 
 const app = express();
 
@@ -18,8 +18,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+//app.use("/users", usersRouter);
 
-app.get("/client", (req, res) => res.sendFile(`${__dirname}/client/client.html`));
 const server = http.createServer(app);
 server.listen(5500, () => console.log("#### 서버 시작 ####"));
