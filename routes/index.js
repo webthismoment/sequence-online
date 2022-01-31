d6e6ba13ae3ef85faa1e0beb364414ae3da3adb8
webthismoment <85307010+webthismoment@users.noreply.github.com>
2022-01-31 18:01:53 +0900

    express starter

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,35 @@
 var express = require('express');
 var router = express.Router();
+const socketio = require("socket.io");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+//socket 
+let player_cnt = 0;
+
+const io = socketio(router);
+io.on("connection", (socket) => {
+    //socket.join(`${}`);
+    //io.to("room1").to("room2").to("room3").emit("some event");
+    
+    const { url } = socket.request;
+    console.log(`연결됨: ${url}`);
+
+    socket.on("text", (text)=> console.log(`메세지: ${text}`));
+
+    socket.on("callback", (data, cb) => {
+        console.log(data);
+        cb({result:false, data:"이미 존재"})
+        return false;
+    });
+    
+    //disconnect
+    socket.on("disconnect", ()=>{
+        console.log("user disconnected");
+    })
 });
 
 module.exports = router;
